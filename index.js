352c9d6dd8776dfa6f40b483d54ab2c1815ed52e
G.Parker <82189292+dontez0626@users.noreply.github.com>
2021-06-21 01:46:48 -0400

    finished questions

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+/** @format */
+
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 
 //const fs = require('fs');
 //const generatePage = require('./src/page-template');
@@ -7,29 +9,85 @@ const inquirer = require('inquirer');
 //const pageHTML = generatePage(name, github);
 
 //fs.writeFile('./index.html', pageHTML, err => {
-  //if (err) throw err;
+//if (err) throw err;
 
-  //console.log('ReadMe complete! Check out index.html to see the output!');
+//console.log('ReadMe complete! Check out index.html to see the output!');
 //});
 
-
 // TODO: Create an array of questions for user input
-inquirer
-  .prompt([
-    { type: "input", message: "What is the project title?", name: "title" },
-  ])
-  .then((answers) => console.log(answers));
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the project title?",
+    },
 
+    {
+      type: "input",
+      name: "questions",
+      message: "What is your E-mail address?",
+    },
 
+    {
+      type: "input",
+      name: "questions",
+      message: " please enetr your Git Hub username",
+    },
 
+    {
+      type: "input",
+      name: "installation",
+      message: "how to install your project",
+    },
 
+    {
+      type: "input",
+      name: "description",
+      message: "What is the description of project?",
+    },
 
+    {
+      type: "input",
+      name: "contributing",
+      message: "who contributed to your project",
+    },
 
+    {
+      type: "input",
+      name: "usage",
+      message:
+        "Provide instructions and examples of your project in use for the Usage section",
+    },
+
+    {
+      type: "input",
+      name: "test",
+      message:
+        "Provide tests for your application and provide examples on how to run them.",
+    },
+
+    {
+      type: "list",
+      message: "Choose a license for your project.",
+      choices: [
+        "GNU AGPLv3",
+        "GNU GPLv3",
+        "GNU LGPLv3",
+        "Mozilla Public License 2.0",
+        "Apache License 2.0",
+        "MIT License",
+        "Boost Software License 1.0",
+        "The Unlicense",
+      ],
+      name: "license",
+    },
+  ]);
+};
+promptUser().then((answers) => console.log(answers));
 
 // TODO: Create a function to write README file
 
 // TODO: Create a function to initialize app
 
-
 // Function call to initialize app
-
