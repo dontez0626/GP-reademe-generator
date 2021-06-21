a209b53bdc8db8be1a0228906d3d418919ad9ae5
G.Parker <82189292+dontez0626@users.noreply.github.com>
2021-06-21 00:19:28 -0400

    first question array

--- index.js
+++ index.js
@@ -14,78 +14,12 @@ const inquirer = require('inquirer');
 
 
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        message:"What is the project title?",
-        name: 'title',
-        validate: (value) => { if (value) { return true } else { return 'please enter value to continue' } },
-    },
+inquirer
+  .prompt([
+    { type: "input", message: "What is the project title?", name: "title" },
+  ])
+  .then((answers) => console.log(answers));
 
-
-    {
-        type: 'input',
-        message:" What is the description of project?",
-        name: 'description',
-        validate: (value) => { if (value) { return true } else { return 'please enter value to continue' } },
-    },
-
-
-    {
-        type: 'input',
-        message: 'how to install your app?',
-        name: 'installation',
-        validate: (value) => { if (value) { return true } else { return ' please enter a value to continue' } }
-    },
-
-    {
-        type: 'input',
-        message: 'Who contributed to your project?',
-        name: 'Contributing ',
-        validate: (value) => { if (value) { return true } else { return ' please enter a value to continue' } }
-    },
-    
-    
-    {
-
-        type: 'input',
-        message: 'License',
-        name: 'License',
-        validate: (value) => {
-            if (value) { return true } else { return ' please enter a value to continue' }}
-        
-    },
-
-    {
-        type: 'input',
-        message: 'E-mail',
-        name: 'questions',
-        validate: (value) => {
-            if (value) { return true } else { return ' please enter a value to continue' }}
-        
-    },
-
-    {
-        type: 'input',
-        message: 'Github username',
-        name: 'questions',
-        validate: (value) => { if (value) { return true } else { return ' please enter a value to continue' } },
-    },
-
-    {
-        type: 'input',
-        message: 'usage',
-        name: 'usage',
-        validate: (value) => { if (value) { return true } else { return ' please enter a value to continue' } },
-    },
-
-    {
-        type: 'input',
-        message: 'Test',
-        name: 'Test',
-        validate: (value) => { if (value) { return true } else { return ' please enter a value to continue' } },
-    },
-]
 
 
 
